50a3a0d3d6b6e4f10d7962b301e914e7219350a9
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-11-22 04:14:49 +0300

    fix

--- temp.py
+++ temp.py
@@ -8,12 +8,12 @@ django.setup()
 from django.db.models import Count
 from property.models import Owner  # Импортируйте модель после инициализации Django
 
-# Получите всех владельцев и подсчитайте количество связанных с ними квартир
+# Получим всех владельцев и подсчитайте количество связанных с ними квартир
 owners = Owner.objects.annotate(num_flats=Count('flats'))
 
-# Фильтруйте владельцев, чтобы получить только тех, у которых больше одной квартиры
+# только те, у которых больше одной квартиры
 owners_with_multiple_flats = owners.filter(num_flats__gt=1)
 
-# Выведите владельцев и количество их квартир
+# Вывод
 for owner in owners_with_multiple_flats:
     print(f'Owner: {owner.full_name}, Number of flats: {owner.num_flats}')
